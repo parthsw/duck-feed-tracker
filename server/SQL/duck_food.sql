d83e62d7408e729ad77ae9c39c6ceee193cb91b4
Parth Parmar <parmar.parthsw@gmail.com>
2020-12-12 01:50:12 -0400

    Initial database schema

---
+++ server/SQL/duck_food.sql
@@ -0,0 +1,8 @@
+USE duck_feeding_tracker;
+
+INSERT INTO duck_food (food_type) VALUES ("Corn");
+INSERT INTO duck_food (food_type) VALUES ("Duck pellets");
+INSERT INTO duck_food (food_type) VALUES ("Lettuce or other greens");
+INSERT INTO duck_food (food_type) VALUES ("Peas");
+INSERT INTO duck_food (food_type) VALUES ("Oats");
+INSERT INTO duck_food (food_type) VALUES ("Seeds");
