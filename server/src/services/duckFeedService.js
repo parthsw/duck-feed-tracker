a060e7b3619924424cb793f57e22180ae609316a
Parth Parmar <parmar.parthsw@gmail.com>
2020-12-14 01:46:36 -0400

    Modified API to fetch subset of feeding dataset

--- server/src/services/duckFeedService.js
+++ server/src/services/duckFeedService.js
@@ -1,12 +1,39 @@
-// Service layer for the duck feed resource communicating with the database, transforming the response, 
+// Service layer for the duck feed resource communicating with the database, transforming the response,
 // and returning the results back to the controller
 
 const mysql = require('mysql');
 const config = require('src/config/index');
 
 class DuckFeedService {
-  getAllDuckFeeds = `SELECT * FROM ${config.database.name}.duck_feed_dataset`;
-  duckFeed = `INSERT INTO ${config.database.name}.duck_feed_dataset(food_type_id, food_description, food_qty_gms, no_of_ducks, country_id, feed_time, feed_date, park_location, is_repetitive, participant_name, participant_email) VALUES(?,?,?,?,?,?,?,?,?,?,?)`;
+  getAllDuckFeeds = `SELECT 
+    A.id,
+    C.food_type, 
+    A.food_description, 
+    A.food_qty_gms, 
+    A.no_of_ducks, 
+    B.country_name, 
+    A.park_location, 
+    A.feed_time, 
+    A.feed_date FROM 
+    ((${config.database.name}.duck_feed_dataset AS A INNER JOIN ${config.database.name}.country as B ON A.country_id = B.id) 
+      INNER JOIN 
+      ${config.database.name}.duck_food as C ON A.food_type_id = C.id);`;
+
+  duckFeed = `INSERT 
+    INTO 
+    ${config.database.name}.duck_feed_dataset(
+      food_type_id, 
+      food_description, 
+      food_qty_gms, 
+      no_of_ducks, 
+      country_id, 
+      feed_time, 
+      feed_date, 
+      park_location, 
+      is_repetitive, 
+      participant_name, 
+      participant_email) 
+      VALUES(?,?,?,?,?,?,?,?,?,?,?)`;
 
   constructor(database) {
     this.database = database;
